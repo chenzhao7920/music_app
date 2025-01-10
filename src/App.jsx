1c8fede8ae63b8b89331c444e1bb8de3f72a15e1
chenzhao7920 <chenzhao7920@gmail.com>
2025-01-10 11:30:41 -0500

    initialize project with vite, react and mui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <h1>Music Discovery App</h1>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
